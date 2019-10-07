3e249d88fe75c5ca216be78f3a376f8f132c517e
Dimas Ragil T <dynastymasra@gmail.com>
2019-10-07 16:32:36 +0700

    Change pow to new algo

--- algorithm/pow.go
+++ algorithm/pow.go
@@ -3,15 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("2 power 2 is ", power(2, 4))
+	fmt.Println("2 power 5 is ", power(2, 5))
 }
 
 func power(x, y int) int {
 	if y == 0 {
 		return 1
-	} else if y%2 == 0 {
-		return power(x, y/2) * power(x, y/2)
-	} else {
-		return x * power(x, y/2) * power(x, y/2)
 	}
+
+	temp := power(x, y/2)
+
+	if y%2 == 0 {
+		return temp * temp
+	}
+
+	return x * temp * temp
 }

9669dc6c3b29dfaa50bebd8d994aaaecbcf95c11
Dimas Ragil T <dynastymasra@gmail.com>
2019-10-07 15:49:57 +0700

    Add power algorithm

--- algorithm/pow.go
+++ algorithm/pow.go
@@ -1,1 +1,17 @@
-package algorithm
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("2 power 2 is ", power(2, 4))
+}
+
+func power(x, y int) int {
+	if y == 0 {
+		return 1
+	} else if y%2 == 0 {
+		return power(x, y/2) * power(x, y/2)
+	} else {
+		return x * power(x, y/2) * power(x, y/2)
+	}
+}
